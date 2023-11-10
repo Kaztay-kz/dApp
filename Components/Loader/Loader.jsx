5ddef75dbfe7dc3ccecd0bddb0d985bbd98958e0
zhandos <zhandosshakimurat@gmail.com>
2023-11-11 02:44:44 +0600

    component update

--- Components/Loader/Loader.jsx
+++ Components/Loader/Loader.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import Style from "./Loader.module.css";
+import images from "../../assets";
 const Loader = () => {
-  return (
-    <div></div>
-  )
+    return (
+        <div className={Style.Loader}>
+            <div className={Style.Loader_box}>
+                <Image src={images.loader}  alt="Load" width={100} height={100}/>
+            </div>
+        </div>
+    )
 }
 
-export default Loader
+
+export default Loader;
