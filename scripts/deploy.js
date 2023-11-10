a7e29c9e939b763999f2012923a3b90b451a9b47
Kaztay-kz <kaztaydaulet@gmail.com>
2023-11-11 02:21:08 +0600

    Deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,10 +8,13 @@ const hre = require("hardhat");
 
 async function main() {
 
-  const ChatApp = await hre.ethers.deployContract("Lock", [], {});
+  const ChatApp = await hre.ethers.getContractFactory("ChatApp");
+  const chatApp = await ChatApp.deploy();
 
-  await lock.waitForDeployment();
+  await chatApp.waitForDeployment();
 
+
+  console.log(' Contract Address: ${chatapp.address}');
 }
 
 // We recommend this pattern to be able to use async/await everywhere
