96b810a347acd6f660e1866b5c746b74fc2f85eb
Kaztay-kz <kaztaydaulet@gmail.com>
2023-11-11 08:00:14 +0600

    API Features

---
+++ Context/ChatAppContext.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { useRouter } from, "next/router";
+
+import { CheckIfWalletConnected, 
+        connectWallet, 
+        connectigWithContract
+} from "../Utils/apiFeature";
+
+
